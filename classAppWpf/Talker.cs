9b0eb2d8807d95e0683bef7403ecd6c6d33808d9
KamilMehring <kamil.mehring@onet.pl>
2021-09-28 14:39:23 +0200

    commit

---
+++ classAppWpf/Talker.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace classUsingApplication_WFP
+{
+    class Talker
+    {
+        public static int BlahBlahBlag(string thingToSay, int numberOfTimes)
+        {
+            string finalString = "";
+            for (int count = 1; count <= numberOfTimes; count++)
+            {
+                finalString = finalString + thingToSay + "\n";
+            }
+            MessageBox.Show(finalString);
+
+            return finalString.Length;
+        }
+    }
