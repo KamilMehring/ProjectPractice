69e071e1605e8be01b36bcb8cba6d427ccb78ed8
KamilMehring <kamil.mehring@onet.pl>
2021-09-28 15:50:52 +0200

    commit

--- Hive management system/Hive management system/NectarCollector.cs
+++ Hive management system/Hive management system/NectarCollector.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Hive_management_system
-{
-    class NectarCollector
-    {
-        public int Nectar { get; set; }
-        public void Findflowers() { }
-        public void gathernectar() { }
-        public void ReturnToHive() { }
-    }
-}
+//namespace Hive_management_system
+//{
+//    class NectarCollector
+//    {
+//        public int Nectar { get; set; }
+//        public void Findflowers() { }
+//        public void gathernectar() { }
+//        public void ReturnToHive() { }
+//    }
+//}
--- Hive management system/Hive management system/StringPatrol.cs
+++ Hive management system/Hive management system/StringPatrol.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Hive_management_system
-{
-    class StringPatrol : Worker
-    {
-        public int AlertLevel { get; private set; }
-        public int StingerLength { get; set; }
-        public bool SharpenStinger(int Length)
-        {
+//namespace Hive_management_system
+//{
+//    class StringPatrol : Worker
+//    {
+//        public int AlertLevel { get; private set; }
+//        public int StingerLength { get; set; }
+//        public bool SharpenStinger(int Length)
+//        {
 
-        } 
-        public bool LookForEnemies() { }
-        public void Sting(string Enemy) { }
+//        } 
+//        public bool LookForEnemies() { }
+//        public void Sting(string Enemy) { }
 
-    }
-}
+//    }
+//}
