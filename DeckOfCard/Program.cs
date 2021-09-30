eb9d5c3e6dcbc2278939529b3c2a0d528f6f9d27
KamilMehring <kamil.mehring@onet.pl>
2021-09-30 14:26:53 +0200

    First commit

---
+++ DeckOfCard/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeckOfCard
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            Card cardToCeck = new Card(Suits.Clubs, Values.Three);
+            bool doesItMatch = Card.DoesCardMatch(cardToCeck, Suits.Hearts);
+            Console.WriteLine(doesItMatch);
+        }
+    }
+}
