7219c9261af6173cef094180b5cc7eb3dd708e17
KamilMehring <kamil.mehring@onet.pl>
2021-10-01 13:00:22 +0200

    First commit

---
+++ GoToFishing/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace GoToFishing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.listHand = new System.Windows.Forms.ListBox();
+            this.textProgress = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBooks = new System.Windows.Forms.TextBox();
+            this.buttonAsk = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(12, 25);
+            this.textName.Multiline = true;
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(217, 23);
+            this.textName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Imię";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(235, 25);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(154, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Rozpocznij gre!";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // listHand
+            // 
+            this.listHand.FormattingEnabled = true;
+            this.listHand.Location = new System.Drawing.Point(395, 25);
+            this.listHand.Name = "listHand";
+            this.listHand.Size = new System.Drawing.Size(174, 563);
+            this.listHand.TabIndex = 3;
+            // 
+            // textProgress
+            // 
+            this.textProgress.Location = new System.Drawing.Point(12, 75);
+            this.textProgress.Multiline = true;
+            this.textProgress.Name = "textProgress";
+            this.textProgress.ReadOnly = true;
+            this.textProgress.Size = new System.Drawing.Size(377, 438);
+            this.textProgress.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Postęp gry";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 526);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Grupy";
+            // 
+            // textBooks
+            // 
+            this.textBooks.Location = new System.Drawing.Point(12, 542);
+            this.textBooks.Multiline = true;
+            this.textBooks.Name = "textBooks";
+            this.textBooks.ReadOnly = true;
+            this.textBooks.Size = new System.Drawing.Size(377, 92);
+            this.textBooks.TabIndex = 7;
+            // 
+            // buttonAsk
+            // 
+            this.buttonAsk.Enabled = false;
+            this.buttonAsk.Location = new System.Drawing.Point(395, 594);
+            this.buttonAsk.Name = "buttonAsk";
+            this.buttonAsk.Size = new System.Drawing.Size(174, 40);
+            this.buttonAsk.TabIndex = 8;
+            this.buttonAsk.Text = "Zażadaj karty";
+            this.buttonAsk.UseVisualStyleBackColor = true;
+            this.buttonAsk.Click += new System.EventHandler(this.buttonAsk_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(402, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Ręka";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 646);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonAsk);
+            this.Controls.Add(this.textBooks);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textProgress);
+            this.Controls.Add(this.listHand);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ListBox listHand;
+        private System.Windows.Forms.TextBox textProgress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBooks;
+        private System.Windows.Forms.Button buttonAsk;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ GoToFishing/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoToFishing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Game game;
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if ( string.IsNullOrEmpty(textName.Name))
+            {
+                MessageBox.Show("Wpisz swoje imię", "Nie można jescze rozpocząć gry.");
+                return;
+            }
+            game = new Game(textName.Text, new List<string> { "Janek", "Bartek" }, textProgress);
+            buttonStart.Enabled = false;
+            textName.Enabled = false;
+            buttonAsk.Enabled = true;
+            UpdateFrom();
+        }
+        private void UpdateFrom()
+        {
+            listHand.Items.Clear();
+            foreach (String cardName in game.GetPlayerCardNames())
+                listHand.Items.Add(cardName);
+            textBooks.Text = game.DescribeBooks();
+            textProgress.Text += game.DescribePlayerHands();
+            textProgress.SelectionStart = textProgress.Text.Length;
+            textProgress.ScrollToCaret();
+        }
+
+        private void buttonAsk_Click(object sender, EventArgs e)
+        {
+            textProgress.Text = "";
+            if (listHand.SelectedIndex < 0)
+            {
+                MessageBox.Show("Wybierz kartę.");
+                return;
+            }
+            if (game.PlayOneRound(listHand.SelectedIndex))
+            {
+                textProgress.Text += "Zwycięzcą jest..." + game.GetWinnerName();
+                textBooks.Text = game.DescribeBooks();
+                buttonAsk.Enabled = false;
+            }
+            else
+                UpdateFrom();
+        }
+    }
+}
