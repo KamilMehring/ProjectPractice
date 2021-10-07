411f902ae6395a90f9bae10683a2fb29531d1c5f
KamilMehring <kamil.mehring@onet.pl>
2021-10-07 11:15:49 +0200

    Second Commit

--- ListOfWorkers/Form1.cs
+++ ListOfWorkers/Form1.cs
@@ -31,16 +31,22 @@ namespace ListOfWorkers
         //show data in table
         private void data_show()
         {
-            var con = new SQLiteConnection(cs);
-            con.Open();
-
-            string stm = "SELECT * FROM test";
-            var cmd = new SQLiteCommand(stm, con);
-            dr = cmd.ExecuteReader();
-
-            while (dr.Read())
+            try
             {
-                dataGridView1.Rows.Insert(0, dr.GetString(0), dr.GetString(1));
+                var con = new SQLiteConnection(cs);
+                con.Open();
+
+                string stm = "SELECT * FROM test";
+                var cmd = new SQLiteCommand(stm, con);
+                dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    dataGridView1.Rows.Insert(0, dr.GetString(0), dr.GetString(1));
+                }
+            } catch
+            {
+
             }
         }
         //create database and table
@@ -49,10 +55,12 @@ namespace ListOfWorkers
             if (!System.IO.File.Exists(path))
             {
                 SQLiteConnection.CreateFile(path);
-                using (var sqlite = new SQLiteConnection("Data Source=hello.db"))
+                using (var sqlite = new SQLiteConnection("Data Source=data_table.db"))
                 {
                     sqlite.Open();
-                    string sql = "create table test(name varchar(20),id varchar(12))";
+                    string sql = @"create table test(name varchar(20),id varchar(12),surname varchar(20), pesel varchar(12), 
+        jobTitle varchar(20) , dateEmp varchar(12) , busniessPhone varchar(12) 
+        ,phone varchar(12) ,adress varcar(20) , salary varchar(12))";
                     SQLiteCommand command = new SQLiteCommand(sql, sqlite);
                     command.ExecuteNonQuery();
                 }
@@ -73,8 +81,8 @@ namespace ListOfWorkers
             try
             {
 
-                cmd.CommandText = "INSERT INTO test(id,name,surname,pesel,jobTitle,dateEmp,busniessPhone,phone,adress,salary) "
-                    + "VALUES(@Name,@Id,@surname,@pesel,@jobTitle,@dateEmp,@busniessPhone,@phone,@adress,@salary)";
+                cmd.CommandText = "INSERT INTO test(id , name , surname , pesel , jobTitle , dateEmp , busniessPhone , phone , adress , salary ) "
+                    + "VALUES( @Name , @Id , @Surname , @Pesel , @JobTitle , @DateEmp , @BusniessPhone , @Phone , @Adress , @Salary )";
 
                 string ID = id.Text;
                 string Name = name.Text;
@@ -89,28 +97,28 @@ namespace ListOfWorkers
 
                 cmd.Parameters.AddWithValue("@Id", ID);
                 cmd.Parameters.AddWithValue("@Name", Name);
-                cmd.Parameters.AddWithValue("@surname", Surname);
-                cmd.Parameters.AddWithValue("@pesel", PESEL);
-                cmd.Parameters.AddWithValue("@jobTitle", JoBTitle);
-                cmd.Parameters.AddWithValue("@dateEmp", DataOfEmp);
-                cmd.Parameters.AddWithValue("@busniessPhone", Bphone);
-                cmd.Parameters.AddWithValue("@phone", Phone);
-                cmd.Parameters.AddWithValue("@adress", Adress);
-                cmd.Parameters.AddWithValue("@salary", Salary);
+                cmd.Parameters.AddWithValue("@Surname", Surname);
+                cmd.Parameters.AddWithValue("@Pesel", PESEL);
+                cmd.Parameters.AddWithValue("@JobTitle", JoBTitle);
+                cmd.Parameters.AddWithValue("@DateEmp", DataOfEmp);
+                cmd.Parameters.AddWithValue("@BusniessPhone", Bphone);
+                cmd.Parameters.AddWithValue("@Phone", Phone);
+                cmd.Parameters.AddWithValue("@Adress", Adress);
+                cmd.Parameters.AddWithValue("@Salary", Salary);
 
 
 
                 dataGridView1.ColumnCount = 10;
                 dataGridView1.Columns[0].Name = "id";
                 dataGridView1.Columns[1].Name = "name";
-                dataGridView1.Columns[2].Name = "Surname";
-                dataGridView1.Columns[3].Name = "PESEL";
-                dataGridView1.Columns[4].Name = "JobTitle";
-                dataGridView1.Columns[5].Name = "Date Of Employent";
-                dataGridView1.Columns[6].Name = "BusniessPhone";
-                dataGridView1.Columns[7].Name = "Phone";
-                dataGridView1.Columns[8].Name = "Adress";
-                dataGridView1.Columns[9].Name = "Salary";
+                dataGridView1.Columns[2].Name = "surname";
+                dataGridView1.Columns[3].Name = "pesel";
+                dataGridView1.Columns[4].Name = "jobTitle";
+                dataGridView1.Columns[5].Name = "dateEmp";
+                dataGridView1.Columns[6].Name = "busniessPhone";
+                dataGridView1.Columns[7].Name = "phone";
+                dataGridView1.Columns[8].Name = "adress";
+                dataGridView1.Columns[9].Name = "salary";
 
 
 
@@ -128,8 +136,9 @@ namespace ListOfWorkers
 
         }
 
-        private void delateWorker_Click(object sender, EventArgs e)
+        private void saveWorker_Click(object sender, EventArgs e)
         {
+
             var con = new SQLiteConnection(cs);
             con.Open();
 
@@ -137,18 +146,52 @@ namespace ListOfWorkers
 
             try
             {
-                cmd.CommandText = "DELETE FROM test where name =@Name";
+                cmd.CommandText = "UPDATE test Set name = @Name  where id = @Id";
                 cmd.Prepare();
                 cmd.Parameters.AddWithValue("@Id", id.Text);
                 cmd.Parameters.AddWithValue("@Name", name.Text);
-                cmd.Parameters.AddWithValue("@surname", surname.Text);
-                cmd.Parameters.AddWithValue("@pesel", pesel.Text);
-                cmd.Parameters.AddWithValue("@jobTitle", jobTitle.Text);
-                cmd.Parameters.AddWithValue("@dateEmp", dateEmp.Text);
-                cmd.Parameters.AddWithValue("@busniessPhone", busniessPhone.Text);
-                cmd.Parameters.AddWithValue("@phone", phone.Text);
-                cmd.Parameters.AddWithValue("@adress", adress.Text);
-                cmd.Parameters.AddWithValue("@salary", salary.Text);
+                //cmd.Parameters.AddWithValue("@surname", surname.Text);
+                //cmd.Parameters.AddWithValue("@pesel", pesel.Text);
+                //cmd.Parameters.AddWithValue("@jobTitle", jobTitle.Text);
+                //cmd.Parameters.AddWithValue("@dateEmp", dateEmp.Text);
+                //cmd.Parameters.AddWithValue("@busniessPhone", busniessPhone.Text);
+                //cmd.Parameters.AddWithValue("@phone", phone.Text);
+                //cmd.Parameters.AddWithValue("@adress", adress.Text);
+                //cmd.Parameters.AddWithValue("@salary", salary.Text);
+
+                cmd.ExecuteNonQuery();
+                dataGridView1.Rows.Clear();
+                data_show();
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("cannot update data");
+                return;
+            }
+        }
+
+        private void delateWorker_Click(object sender, EventArgs e)
+        {
+            var con = new SQLiteConnection(cs);
+            con.Open();
+
+            //var cmd = new SQLiteCommand(con);
+
+            try
+            {
+                var cmd = con.CreateCommand(); cmd.CommandText = "DELETE FROM test where id =@Id";
+                //cmd.Prepare();
+                cmd.Parameters.AddWithValue("@Id", id.Text);
+                cmd.Parameters.AddWithValue("@Name", name.Text);
+                cmd.Parameters.AddWithValue("@Surname", surname.Text);
+                cmd.Parameters.AddWithValue("@Pesel", pesel.Text);
+                cmd.Parameters.AddWithValue("@JobTitle", jobTitle.Text);
+                cmd.Parameters.AddWithValue("@DateEmp", dateEmp.Text);
+                cmd.Parameters.AddWithValue("@BusniessPhone", busniessPhone.Text);
+                cmd.Parameters.AddWithValue("@Phone", phone.Text);
+                cmd.Parameters.AddWithValue("@Adress", adress.Text);
+                cmd.Parameters.AddWithValue("@Salary", salary.Text);
 
                 cmd.ExecuteNonQuery();
                 dataGridView1.Rows.Clear();
@@ -163,22 +206,22 @@ namespace ListOfWorkers
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            {
+            
                 if (dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
                 {
                     dataGridView1.CurrentRow.Selected = true;
-                    id.Text = dataGridView1.Rows[e.RowIndex].Cells["Id"].FormattedValue.ToString();
-                    name.Text = dataGridView1.Rows[e.RowIndex].Cells["Name"].FormattedValue.ToString();
-                    surname.Text = dataGridView1.Rows[e.RowIndex].Cells["Surname"].FormattedValue.ToString();
-                    pesel.Text = dataGridView1.Rows[e.RowIndex].Cells["Pesel"].FormattedValue.ToString();
-                    jobTitle.Text = dataGridView1.Rows[e.RowIndex].Cells["JobTitle"].FormattedValue.ToString();
-                    dateEmp.Text = dataGridView1.Rows[e.RowIndex].Cells["Date Of Employent"].FormattedValue.ToString();
-                    busniessPhone.Text = dataGridView1.Rows[e.RowIndex].Cells["BusniessPhone"].FormattedValue.ToString();
-                    phone.Text = dataGridView1.Rows[e.RowIndex].Cells["Phone"].FormattedValue.ToString();
-                    adress.Text = dataGridView1.Rows[e.RowIndex].Cells["Adress"].FormattedValue.ToString();
-                    salary.Text = dataGridView1.Rows[e.RowIndex].Cells["Salary"].FormattedValue.ToString();
+                    id.Text = dataGridView1.Rows[e.RowIndex].Cells["id"].FormattedValue.ToString();
+                    name.Text = dataGridView1.Rows[e.RowIndex].Cells["name"].FormattedValue.ToString();
+                    surname.Text = dataGridView1.Rows[e.RowIndex].Cells["surname"].FormattedValue.ToString();
+                    pesel.Text = dataGridView1.Rows[e.RowIndex].Cells["pesel"].FormattedValue.ToString();
+                    jobTitle.Text = dataGridView1.Rows[e.RowIndex].Cells["jobTitle"].FormattedValue.ToString();
+                    dateEmp.Text = dataGridView1.Rows[e.RowIndex].Cells["dateEmp"].FormattedValue.ToString();
+                    busniessPhone.Text = dataGridView1.Rows[e.RowIndex].Cells["busniessPhone"].FormattedValue.ToString();
+                    phone.Text = dataGridView1.Rows[e.RowIndex].Cells["phone"].FormattedValue.ToString();
+                    adress.Text = dataGridView1.Rows[e.RowIndex].Cells["adress"].FormattedValue.ToString();
+                    salary.Text = dataGridView1.Rows[e.RowIndex].Cells["salary"].FormattedValue.ToString();
                 }
-            }
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -187,42 +230,6 @@ namespace ListOfWorkers
             Create_db();
             data_show();
 
-        }
-
-        private void saveWorker_Click(object sender, EventArgs e)
-        {
-            {
-                var con = new SQLiteConnection(cs);
-                con.Open();
-
-                var cmd = new SQLiteCommand(con);
-
-                try
-                {
-                    cmd.CommandText = "UPDATE test Set id=@Id , where name=@Name";
-                    cmd.Prepare();
-                    //cmd.Parameters.AddWithValue("@Id", id.Text);
-                    cmd.Parameters.AddWithValue("@Name", name.Text);
-                    //cmd.Parameters.AddWithValue("@surname", surname.Text);
-                    //cmd.Parameters.AddWithValue("@pesel", pesel.Text);
-                    //cmd.Parameters.AddWithValue("@jobTitle", jobTitle.Text);
-                    //cmd.Parameters.AddWithValue("@dateEmp", dateEmp.Text);
-                    //cmd.Parameters.AddWithValue("@busniessPhone", busniessPhone.Text);
-                    //cmd.Parameters.AddWithValue("@phone", phone.Text);
-                    //cmd.Parameters.AddWithValue("@adress", adress.Text);
-                    //cmd.Parameters.AddWithValue("@salary", salary.Text);
-
-                    cmd.ExecuteNonQuery();
-                    dataGridView1.Rows.Clear();
-                    data_show();
-
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("cannot update data");
-                    return;
-                }
-            }
         }
 
         private void generate_Click(object sender, EventArgs e)
